3638d73be4b284503a6649d7f886210273f14c8f
sven <sven.song@qq.com>
2025-11-09 09:41:45 +0800

    todo:异常

--- day13_异常.py
+++ day13_异常.py
@@ -1,2 +1,21 @@
-# 猜数字异常
+# todo: 猜数字异常
+import random
+
+userNum = int( input('请输入你猜的数字'))
+
+target = random.randint(1, 100)
+
+while True:
+
+  if target == userNum:
+    print('命中！！')
+    break
+  elif target > userNum:
+    print('太小了')
+    userNum = int( input('请输入你猜的数字'))
+
+
+  else :
+    print('太大了')
+    userNum = int( input('请输入你猜的数字'))
 
