52812b83de072bf1106287708ea920e9bd947f3e
sven <sven.song@qq.com>
2025-11-11 09:41:18 +0800

    模块

--- day13_异常01.py
+++ day13_异常01.py
@@ -1,28 +1,34 @@
 # todo: 猜数字异常
-import random
-userNum = 0
-target = random.randint(1, 100)
+# import random
+# userNum = 0
+# target = random.randint(1, 100)
+#
+# try:
+#   userNum = int( input('请输入你猜的数字'))
+#
+#
+#
+#   while True:
+#
+#     if target == userNum:
+#       print('命中！！')
+#       break
+#     elif target > userNum:
+#       print('太小了')
+#       userNum = int(input('请输入你猜的数字'))
+#
+#
+#     else:
+#       print('太大了')
+#       userNum = int(input('请输入你猜的数字'))
+# except:
+#   print('请输入数字!!!')
+#   userNum = int(input('请输入你猜的数字'))
 
-try:
-  userNum = int( input('请输入你猜的数字'))
 
+# import day13_模块 as mod
+#
+# print(mod.sum(3,4))
 
-
-  while True:
-
-    if target == userNum:
-      print('命中！！')
-      break
-    elif target > userNum:
-      print('太小了')
-      userNum = int(input('请输入你猜的数字'))
-
-
-    else:
-      print('太大了')
-      userNum = int(input('请输入你猜的数字'))
-except:
-  print('请输入数字!!!')
-  userNum = int(input('请输入你猜的数字'))
-
-
+import day13_模块
+print(day13_模块.sum(1,4))
