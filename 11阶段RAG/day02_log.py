53b51fde844c7f92ef894e2b22f9cb56d17eaf6e
sven <sven.song@qq.com>
2025-11-29 10:32:28 +0800

    示例3：将日志存储到文件

--- 11阶段RAG/day02_log.py
+++ 11阶段RAG/day02_log.py
@@ -17,18 +17,36 @@ import logging
 # ------------demo2--------------------
 
 # 配置日志格式
+# logging.basicConfig(
+#     level=logging.DEBUG,
+#     format='%(asctime)s - %(levelname)s - %(message)s'
+# )
+
+# # 获取日志记录器
+# logger = logging.getLogger("Example2")
+
+# # 记录日志
+# logger.debug("调试模式已开启")
+# logger.info("正在处理数据")
+# logger.error("数据处理失败")
+
+# ------------demo3--------------------
+
+# todo  示例3：将日志存储到文件
+
+
+# 配置日志，输出到文件
 logging.basicConfig(
-    level=logging.DEBUG,
-    format='%(asctime)s - %(levelname)s - %(message)s'
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    filename='app.log',  # 日志文件路径
+    filemode='a'         # 'a'表示追加，'w'表示覆盖
 )
 
 # 获取日志记录器
-logger = logging.getLogger("Example2")
+logger = logging.getLogger("Example3")
 
 # 记录日志
-logger.debug("调试模式已开启")
-logger.info("正在处理数据")
-logger.error("数据处理失败")
-
-
-# todo  示例3：将日志存储到文件
+logger.info("程序启动!!")
+logger.warning("内存使用率较高")
+logger.error("无法连接数据库")
